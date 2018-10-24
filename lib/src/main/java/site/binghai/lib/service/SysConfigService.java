5edb7e5b7bd0c567f48eda645af68642d22c43f2
IceSeaOnly <1041414957@qq.com>
2018-10-24 14:33:58 +0800

    update

--- lib/src/main/java/site/binghai/lib/service/SysConfigService.java
+++ lib/src/main/java/site/binghai/lib/service/SysConfigService.java
@@ -3,30 +3,35 @@ package site.binghai.lib.service;
 import org.springframework.stereotype.Service;
 import site.binghai.lib.entity.SysConfig;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 public class SysConfigService extends BaseService<SysConfig> {
     public boolean isSystemClosed() {
-        List<SysConfig> configList = findAll(999);
-        if (isEmptyList(configList)) {
-            return false;
-        }
-        SysConfig config = configList.get(0);
-        return config.getCloseSystem() == null ? false : config.getCloseSystem();
+        SysConfig config = findById(1L);
+        return config == null ? false : config.getCloseSystem();
     }
 
     public String getCloseReason() {
-        List<SysConfig> configList = findAll(999);
-        if (isEmptyList(configList)) {
-            return "系统临时关闭";
-        }
-        SysConfig config = configList.get(0);
-        return config.getCloseMessage() == null ? "系统临时关闭" : config.getCloseMessage();
+        SysConfig config = findById(1L);
+        return config == null ? "系统临时关闭" : config.getCloseMessage();
     }
 
+    public SysConfig getConfig(){
+        SysConfig sysConfig = findById(1L);
+        if(sysConfig == null){
+            sysConfig = new SysConfig();
+            sysConfig.setCloseSystem(false);
+            sysConfig.setCloseMessage("系统临时关闭");
+            return save(sysConfig);
+        }
+        return sysConfig;
+    }
+
+    @Transactional
     public void setSystem(SysConfig config) {
-        deleteAll("confirm");
-        save(config);
+        config.setId(1L);
+        update(config);
     }
 }
