1a648bb8254a434695f5666ec84a8b1ded315adf
huaishuo.lz <huaishuo.lz@alibaba-inc.com>
2018-11-06 14:23:33 +0800

    update

--- biz/src/main/java/site/binghai/biz/service/WxTplMessageService.java
+++ biz/src/main/java/site/binghai/biz/service/WxTplMessageService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import site.binghai.biz.service.jdy.WxTplMsgLogService;
 import site.binghai.biz.tasks.SendTemplateMsg;
+import site.binghai.lib.service.AccessTokenService;
 import site.binghai.lib.utils.BaseBean;
 
 @Service
@@ -13,11 +14,23 @@ public class WxTplMessageService extends BaseBean {
     @Autowired
     private WxTplMsgLogService wxTplMsgLogService;
 
-
-    public String send(JSONObject content){
-        String ret =  new SendTemplateMsg(content).send();
-        wxTplMsgLogService.save(content,ret);
+    public String send(JSONObject content) {
+        String ret = new SendTemplateMsg(content).send();
+        if (hasEmptyString(ret) || accessTokenError(ret)) {
+            logger.warn("access token invalide, reloading...");
+            AccessTokenService.getServiceHolder().immediateExpired();
+            ret = new SendTemplateMsg(content).send();
+        }
+        wxTplMsgLogService.save(content, ret);
         return ret;
+    }
+
+    private boolean accessTokenError(String ret) {
+        JSONObject json = JSONObject.parseObject(ret);
+        if (null != json && !"0".equals(json.getString("errcode"))) {
+            return true;
+        }
+        return false;
     }
 
 }
